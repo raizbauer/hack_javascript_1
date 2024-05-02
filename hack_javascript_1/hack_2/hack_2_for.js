4c579e41a51ec3356055486776cc9330e5707a8d
Miguel Carrera <miguelcarrera12@gmail.com>
2024-05-02 00:52:38 -0400

    hack_2 complete

--- hack_javascript_1/hack_2/hack_2_for.js
+++ hack_javascript_1/hack_2/hack_2_for.js
@@ -7,6 +7,9 @@
  */
 let result = [];
 
+for(let i = 1; i<= 5; i++){
+    result.push(i);
+}
 
 //export result
 module.exports = result;
--- hack_javascript_1/hack_2/hack_2_while.js
+++ hack_javascript_1/hack_2/hack_2_while.js
@@ -6,7 +6,12 @@
  * output => [1,2,3,4,5]
  */
 let result = [];
+let contador = 1;
 
+while (contador < 6){
+    result.push(contador);
+    contador++;
+}
 
 //export result
 module.exports = result;
