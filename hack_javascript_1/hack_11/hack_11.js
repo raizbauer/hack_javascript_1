bc79c9ccc597d52deda838357495b7b5c3e88841
Miguel Carrera <miguelcarrera12@gmail.com>
2024-05-06 17:40:58 -0400

    Completed hack 11

--- hack_javascript_1/hack_11/hack_11.js
+++ hack_javascript_1/hack_11/hack_11.js
@@ -20,5 +20,40 @@ let stringArray = ["foo","bar","baz","qux","echo"]
 let result = [];
 
 
+result = numberArray.concat(stringArray);
+
+for (let i = 0; i < result.length; i++) {
+    if (result[i] === 1) {
+      result[i] = "one";
+    } else if (result[i] === 3) {
+      result[i] = "three";
+    } else if (result[i] === 5) {
+      result[i] = "five";
+    }
+  }
+
+for (let i = 0; i < result.length; i++) {
+    if (typeof result[i] === "string") {
+      if (result[i] === "foo") {
+        result[i] = "f00";
+      } else if (result[i] === "bar") {
+        result[i] = "Bar";
+      } else if (result[i] === "baz") {
+        result[i] = "b@z";
+      } else if (result[i] === "qux") {
+        result[i] = "quX";
+      } else if (result[i] === "echo") {
+        result[i] = "3ch0";
+      }
+    }
+  }
+
+///result.unshift("h@ck");
+
+result.splice(0, 0, "h@ck");
+result.splice(6, 0, "h@ck");
+result.push("h@ck");
+console.log(result);
+
 //export result
 module.exports = result;
